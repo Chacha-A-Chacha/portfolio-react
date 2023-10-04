2fb15fe1361ed23ad72b4096b4d5696b4281c2ab
Chacha Chacha <calex2607@gmail.com>
2023-10-04 23:54:43 +0000

    modified:   src/Components/About.jsx 	modified:   src/Components/Home.jsx 	modified:   src/Components/Portfolio.jsx 	new file:   src/images/john-towner-JgOeRuGD_Y4-unsplash.jpg 	new file:   src/images/kelly-sikkema-o2TRWThve_I-unsplash.jpg 	new file:   src/images/radowan-nakif-rehan-cYyqhdbJ9TI-unsplash.jpg

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "https://images.unsplash.com/photo-1610563166150-b34df4f3bcd6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1376&q=80";
+import image from "../images/radowan-nakif-rehan-cYyqhdbJ9TI-unsplash.jpg";
 
 const imageAltText = "code";
 
